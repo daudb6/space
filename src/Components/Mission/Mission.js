fa122d4e6b3c1083b8be393d69a9eeb87f67a19d
Javed-Shahbaz <javedshahbaz007@gmail.com>
2024-02-14 00:26:58 +0500

    api data fetched

--- src/Components/Mission/Mission.js
+++ src/Components/Mission/Mission.js
@@ -1,39 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Mission.css";
 export const Mission = () => {
+  let [data, setData] = useState(null);
+  const fetchMission = async () => {
+    const mData = await fetch("https://api.spacexdata.com/v3/missions");
+    setData(await mData.json());
+    //console.log(data);
+  };
+  fetchMission();
   return (
-    <div className="main-table">
-      <table className="mission-table">
-        <thead>
-          <tr>
-            <th>Mission</th>
-            <th>Description</th>
-            <th>Status</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Thaicom</td>
-            <td className="justi">
-              Thaicom is the name of a series of communications satellites
-              operated from Thailand, and also the name of Thaicom Public
-              Company Limited, which is the company that owns and operates the
-              Thaicom satellite fleet and other telecommunication businesses in
-              Thailand and throughout the Asia-Pacific region. The satellite
-              projects were named Thaicom by the King of Thailand, His Majesty
-              the King Bhumibol Adulyadej, as a symbol of the linkage between
-              Thailand and modern communications technology.
-            </td>
-            <td>
-              <p>Not a Member</p>
-            </td>
-            <td>
-              <button>Join Mission</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      {!data ? (
+        <h1>Loading......</h1>
+      ) : (
+        <>
+          <div className="main-table">
+            <table className="mission-table">
+              <thead>
+                <tr>
+                  <th>Mission</th>
+                  <th>Description</th>
+                  <th>Status</th>
+                  <th></th>
+                </tr>
+              </thead>
+              {data.map((d) => {
+                return (
+                  <tbody>
+                    <tr>
+                      <td>{d.mission_name}</td>
+                      <td className="justi">{d.description}</td>
+                      <td>
+                        <p>Not a Member</p>
+                      </td>
+                      <td>
+                        <button>Join Mission</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+            </table>
+          </div>
+        </>
+      )}
+    </>
   );
 };
