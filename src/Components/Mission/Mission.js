da3bcdb262a1af1e56e4383cf4b3be8332b5df38
Javed-Shahbaz <javedshahbaz007@gmail.com>
2024-02-13 22:04:53 +0500

    update missions

--- src/Components/Mission/Mission.js
+++ src/Components/Mission/Mission.js
@@ -4,20 +4,35 @@ export const Mission = () => {
   return (
     <div className="main-table">
       <table className="mission-table">
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-          <th></th>
-        </tr>
-        <tr>
-          <td>Mission 1</td>
-          <td>Description</td>
-          <td>Status</td>
-          <td>
-            <button>Join Mission</button>
-          </td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Thaicom</td>
+            <td className="justi">
+              Thaicom is the name of a series of communications satellites
+              operated from Thailand, and also the name of Thaicom Public
+              Company Limited, which is the company that owns and operates the
+              Thaicom satellite fleet and other telecommunication businesses in
+              Thailand and throughout the Asia-Pacific region. The satellite
+              projects were named Thaicom by the King of Thailand, His Majesty
+              the King Bhumibol Adulyadej, as a symbol of the linkage between
+              Thailand and modern communications technology.
+            </td>
+            <td>
+              <p>Not a Member</p>
+            </td>
+            <td>
+              <button>Join Mission</button>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
